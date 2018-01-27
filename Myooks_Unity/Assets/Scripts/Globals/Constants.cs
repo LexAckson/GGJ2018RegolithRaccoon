fb11795fb836158adb5c52ab9b86bb4b27e52588
zephlord <jacob.l.rosenbloom@gmail.com>
2018-01-27 02:22:04 -0500

    Made Player Controller

---
+++ Myooks_Unity/Assets/Scripts/Globals/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Constants
+{
+	public const string HORIZONTAL_AXIS = "Horizontal";
+	public const string VERTICAL_AXIS = "Vertical";
+	public const KeyCode MOVE_LEFT = KeyCode.A;
+	public const KeyCode MOVE_RIGHT = KeyCode.D;
+	public const KeyCode PIN_DROP = KeyCode.Q;
+
+	public const float MOVE_SPEED = .05f; 
+	public const int NEEDLE_COUNT = 3;
+}
---
+++ Myooks_Unity/Assets/Scripts/Thread.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thread : MonoBehaviour {
+
+	public GameObject _startingPoint;
+	public GameObject _endPoint;
+
+	private bool _isConnected;
+	void Update () {
+		
+	}
+}
