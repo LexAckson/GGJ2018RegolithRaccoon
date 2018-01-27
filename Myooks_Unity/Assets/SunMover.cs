99c9969ca8c7cb66a6c1c8ea6456913e4e6ed272
LexAckson <LexAckson@gmail.com>
2018-01-27 00:30:24 -0500

    Added a sun and a Planet Disc

---
+++ Myooks_Unity/Assets/SunMover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunMover : MonoBehaviour {
+
+    private Transform tf;
+    public float sunSpeed;
+
+	// Use this for initialization
+	void Start () {
+        tf = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        tf.position = Quaternion.Euler(0.0f,sunSpeed, 0.0f) * tf.position;
+	}
+}
