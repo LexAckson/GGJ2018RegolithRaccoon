e37972dbc775a7fb6f6852f04157a106ea427af2
LexAckson <LexAckson@gmail.com>
2018-01-27 02:35:30 -0500

    Recommit stuff without .meta

---
+++ Myooks_Unity/Assets/Scripts/MyceliumController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mycelium : MonoBehaviour {
+
+    public GameObject myceliumParticle;
+    public Color myceliumColor;
+    public static float r = 3;
+    private LineRenderer lineRenderer;
+    public int numberOfParticles = 30; //Total number of points in mycelium.
+    public float startWidth = 0.1F;
+    public List<GameObject> myceliumDots;
+    public string mycelium_layer;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Myooks_Unity/Assets/Scripts/SunMover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunMover : MonoBehaviour {
+
+    [SerializeField]
+    private Transform tf;
+    //private Transform ground;
+
+    public float sunSpeed;
+
+	// Use this for initialization
+	void Start () {
+        tf = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        //tf.RotateAround(ground.position, Vector3.up, sunSpeed);
+        tf.Rotate(Vector3.up, sunSpeed); 
+        tf.position = Quaternion.Euler(0.0f,sunSpeed, 0.0f) * tf.position;
+	}
+}
