3cd8f6ea1ba55c64d79c5d0ee83756d2361f9fa6
Jonathan Carr <jcarr031@fiu.edu>
2018-01-27 14:11:27 -0500

    moving things Better

--- Sandbox_Unity/Assets/CamerLogic.cs
+++ Sandbox_Unity/Assets/CamerLogic.cs
@@ -7,10 +7,9 @@ public class CamerLogic : MonoBehaviour {
     public Vector3 myPos;
     public Transform myPlay;
     public Camera mainCamera;
-    public float xAxis = 0.0F;
     public float yAxis = 0.0F;
-    public float xSpeed = 2.0F;
     public float ySpeed = 2.0F;
+
 
     // Use this for initialization
     void Start () {
@@ -21,12 +20,8 @@ public class CamerLogic : MonoBehaviour {
 	void Update () {
 
         transform.position = myPlay.position + myPos;
-
-        xAxis += xSpeed * Input.GetAxis("Mouse X");
         yAxis -= ySpeed * Input.GetAxis("Mouse Y");
-        //mainCamera.transform.Rotate(new Vector3(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0) * Time.deltaTime * 1.0F);
-        transform.eulerAngles = new Vector3(yAxis, xAxis, 0);
-        Debug.Log(Input.GetAxis("Mouse Y"));
-        Debug.Log(Input.GetAxis("Mouse X"));
+        transform.eulerAngles = new Vector3(yAxis, 0, 0);
+
     }
 }
