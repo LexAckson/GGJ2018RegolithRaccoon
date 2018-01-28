9a173b491af0efe69746b088b610a42c50d8c731
zephlord <jacob.l.rosenbloom@gmail.com>
2018-01-27 22:01:53 -0500

    tree and factor add

--- Myooks_Unity/Assets/Scripts/Bug.cs
+++ Myooks_Unity/Assets/Scripts/Bug.cs
@@ -6,24 +6,62 @@ public class Bug : MonoBehaviour {
 
 	public bugColor _color;
 	public GreenTree _targetTree;
-	public float _landingTimer;
-	public bool _isAttakced;
+	public bool _isAttacked;
 	public float _attackTimer;
-	void Start () {
+	private float _eatTimer;
+	private bool _isLanded;
+	private Vector3 _startPos;
+	void Start () 
+	{
+		transform.position = _targetTree.transform.position + 
+				_targetTree.transform.position.normalized * Constants.BUG_START_DIST; 
+		_startPos = transform.position;
+		StartCoroutine(land());
 		
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		
+		if(_isLanded)
+		{
+			_eatTimer += Time.deltaTime;
+			if(_eatTimer >= Constants.BUG_EAT_TIME)
+			{
+				_targetTree.removeResource<Leaf>();
+				_eatTimer = 0;
+			}
+		}
+		if(_isAttacked)
+		{
+			_attackTimer+= Time.deltaTime;
+			if(_attackTimer >= Constants.BUG_DIE_TIME)
+			{
+				_isLanded = false;
+				_targetTree.killBug(this);
+				die();
+			}
+		}
 	}
 
 	public void die()
 	{
-
+		// TODO:
+		// animation for dead bug
+		Destroy(gameObject);
 	}
 
+	private IEnumerator land()
+	{
+		float timer = 0;
+		while(timer / Constants.BUG_DROP_TIME < 1)
+		{
+			timer += Time.deltaTime;
+			transform.position = Vector3.Lerp(_startPos, _targetTree.transform.position, timer / Constants.BUG_DROP_TIME);
+			yield return null;
+		}
+		_isLanded = true;
+	}
 	public void setColor(bugColor color)
 	{
 		_color = color;
